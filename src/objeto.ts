086006e817e63a3169cbb2bca5fdb98ce259bfe0
joaocastro <joaocastro0429@gmail.com>
2024-07-21 22:58:25 -0300

    add objeto e interface

---
+++ src/objeto.ts
@@ -0,0 +1,25 @@
+interface Pessoa{
+    nome:string,
+    idade:number
+    profissao?:string
+  
+}
+
+
+const pessoa= <Pessoa>{
+ nome,
+ idade
+}
+
+pessoa.nome
+pessoa.idade
+
+// array de numeros 
+const arrayNumener:number[]=[
+  1,2,3
+]
+
+// array de string 
+const arrayNumener2: Array<string>=[
+    "1", "2" , "3"
+]
