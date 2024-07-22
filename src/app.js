be6553ab3104a68415b51eabbfbbf99350e59e8b
joaocastro <joaocastro0429@gmail.com>
2024-07-21 21:59:05 -0300

    treimento fullstack typescript

---
+++ src/app.js
@@ -0,0 +1,4 @@
+var soma1 = function (a, b) {
+    return a + b;
+};
+console.log(soma1(10, 20));
---
+++ src/app.ts
@@ -0,0 +1,6 @@
+const soma1= (a:number,b:number)=>{
+    return a+b
+
+}
+
+console.log(soma1(10,20))
